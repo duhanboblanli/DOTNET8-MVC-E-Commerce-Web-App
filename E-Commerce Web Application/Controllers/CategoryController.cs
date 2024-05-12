390cca3bdea025e9b741598293729c634e0ea538
Duhan Boblanlı <77344408+duhanboblanli@users.noreply.github.com>
2024-05-13 02:17:18 +0300

    Add Category Create,Edit,Delete Add Bootstrap

--- E-Commerce Web Application/Controllers/CategoryController.cs
+++ E-Commerce Web Application/Controllers/CategoryController.cs
@@ -1,12 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Commerce_Web_Application.Data;
+using E_Commerce_Web_Application.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce_Web_Application.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoryController : Controller   
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+
+            if (obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            { 
+            return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
--- E-Commerce Web Application/Models/Category.cs
+++ E-Commerce Web Application/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace E_Commerce_Web_Application.Models
 {
@@ -6,8 +7,12 @@ namespace E_Commerce_Web_Application.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required] 
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
         public int DisplayOrder { get; set; }
 
     }
