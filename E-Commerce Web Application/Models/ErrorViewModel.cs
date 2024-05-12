70160cf28cd7668480f21b643329796a50bbc507
Duhan Boblanlı <77344408+duhanboblanli@users.noreply.github.com>
2024-05-12 13:32:05 +0300

    Proje dosyası ekle.

---
+++ E-Commerce Web Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Commerce_Web_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
